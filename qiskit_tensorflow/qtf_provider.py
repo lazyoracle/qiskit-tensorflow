cd85c663bce244b56fc82f2ad1aee01c6965494c
lazyoracle <contact@anuragsaharoy.me>
2020-12-25 22:38:58 +0530

    update naming

--- qiskit_tensorflow/qtf_provider.py
+++ qiskit_tensorflow/qtf_provider.py
@@ -1,12 +1,12 @@
 from qiskit.providers import ProviderV1 as Provider
-from .qtf_backend import QTFQasmSimulator as QasmSimulator  # type: ignore
+from .qtf_backend import QTFQasmSimulator  # type: ignore
 
 
 class QTFProvider(Provider):
     def __init__(self, token=None):
         super().__init__()
         self.name = "qtf_provider"
-        self.backends = [QasmSimulator(provider=self)]
+        self.backends = [QTFQasmSimulator(provider=self)]
 
     def backends(self, name=None, **kwargs):
         if name:
