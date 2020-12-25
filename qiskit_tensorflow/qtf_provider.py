8bffa3a9f43f45bce3a6cc16f951df11fd948d8c
lazyoracle <contact@anuragsaharoy.me>
2020-12-26 01:01:50 +0530

    remove subclassing, follow aqt style

--- qiskit_tensorflow/qtf_provider.py
+++ qiskit_tensorflow/qtf_provider.py
@@ -1,14 +1,76 @@
-from qiskit.providers import ProviderV1 as Provider
-from .qtf_backend import QTFQasmSimulator  # type: ignore
+from qiskit.providers.providerutils import filter_backends
+from .qtf_backend import QTFQasmSimulator
+from qiskit.providers.exceptions import QiskitBackendNotFoundError
 
 
-class QTFProvider(Provider):
-    def __init__(self, token=None):
+class QTFProvider:
+    def __init__(self):
         super().__init__()
-        self.name = "qtf_provider"
-        self.backends = [QTFQasmSimulator(provider=self)]
 
-    def backends(self, name=None, **kwargs):
+        self.name = "qtf_provider"
+        self.backends = BackendService([QTFQasmSimulator(provider=self)])
+
+    def __str__(self):
+        return "<QTFProvider(name={})>".format(self.name)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_backend(self, name=None, **kwargs):
+        """Return a single backend matching the specified filtering.
+        Args:
+            name (str): name of the backend.
+            **kwargs: dict used for filtering.
+        Returns:
+            Backend: a backend matching the filtering.
+        Raises:
+            QiskitBackendNotFoundError: if no backend could be found or
+                more than one backend matches the filtering criteria.
+        """
+        backends = self.backends(name, **kwargs)
+        if len(backends) > 1:
+            raise QiskitBackendNotFoundError("More than one backend matches criteria.")
+        if not backends:
+            raise QiskitBackendNotFoundError("No backend matches criteria.")
+
+        return backends[0]
+
+    def __eq__(self, other):
+        """Equality comparison.
+        By default, it is assumed that two `Providers` from the same class are
+        equal. Subclassed providers can override this behavior.
+        """
+        return type(self).__name__ == type(other).__name__
+
+
+class BackendService:
+    """A service class that allows for autocompletion
+    of backends from provider.
+    """
+
+    def __init__(self, backends):
+        """Initialize service
+
+        Parameters:
+            backends (list): List of backend instances.
+        """
+        self._backends = backends
+        for backend in backends:
+            setattr(self, backend.name(), backend)
+
+    def __call__(self, name=None, filters=None, **kwargs):
+        """A listing of all backends from this provider.
+
+        Parameters:
+            name (str): The name of a given backend.
+            filters (callable): A filter function.
+
+        Returns:
+            list: A list of backends, if any.
+        """
+        # pylint: disable=arguments-differ
+        backends = self._backends
         if name:
-            backends = [backend for backend in self.backends if backend.name() == name]
-            return backends
+            backends = [backend for backend in backends if backend.name() == name]
+
+        return filter_backends(backends, filters=filters, **kwargs)
