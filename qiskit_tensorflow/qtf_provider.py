e836d071f274b8c05adbf6f0a97491cd06ed871d
lazyoracle <contact@anuragsaharoy.me>
2020-12-25 15:37:17 +0530

    barebones provider

---
+++ qiskit_tensorflow/qtf_provider.py
@@ -0,0 +1,14 @@
+from qiskit.providers import ProviderV1 as Provider
+from .qtf_backend import QasmSimulator  # type: ignore
+
+
+class QTFProvider(Provider):
+    def __init__(self, token=None):
+        super().__init__()
+        self.name = "qtf_provider"
+        self.backends = [QasmSimulator(provider=self)]
+
+    def backends(self, name=None, **kwargs):
+        if name:
+            backends = [backend for backend in self.backends if backend.name() == name]
+            return backends
